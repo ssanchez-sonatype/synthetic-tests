444ba71bf88f4027f75ec1965c2f195de9f821a0
Daniel Reimer <daniel@muse.dev>
2020-04-06 09:27:44 -0700

    test
    Signed-off-by: Daniel Reimer <daniel@muse.dev>

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function sayHello() {
-    alert("Hello there");;
+    alert("Hello there line too long line too long line too long line too long line too long line too long line too long line too long");;
 }
 
 sayHello
