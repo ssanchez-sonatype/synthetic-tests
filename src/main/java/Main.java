d159f609203985c4989d1c0c36d3c85dbb7c9d8f
Matt Bauer <mbauer@galois.com>
2021-01-28 06:43:35 -0500

    remove try with resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("/foo/bar/DukesDiary.txt"))) {
+        FileReader fileReader = null;
+        BufferReader reader = null;
 
+        try {
+
+            fileReader = new FileReader("/foo/bar/DukesDiary.txt");
+            reader = new BufferedReader(fileReader)
             String taint = reader.readLine();
             MyWriter.writeValue(taint);
 
+        } catch (Exception e) {
+
+            if (fileReader != null) {
+                fileReader.close();
+            }
+
+            if (reader != null) {
+                reader.close();
+            }
         }
     }
 }
