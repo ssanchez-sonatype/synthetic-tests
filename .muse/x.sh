cc5cfae06cc84d0f5bd47017e54ecb62e096d0fa
Amelia Keibler <akeibler@sonatype.com>
2021-06-29 10:53:37 -0400

    Add pull request functionality to v3 tool
    This branch is intended to replace the v3-tool-run-and-finish branch once the
    LIFT-1265 is merged
    
    LIFT-1263

--- .muse/x.sh
+++ .muse/x.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -e
+
 function tellApplicable() {
     printf "true\n" "$res"
 }
@@ -27,14 +30,28 @@ function finalize() {
         echo "Args: $*" >> /tmp/x.sh.finalize.log
         printf "Stdin: " >> /tmp/x.sh.finalize.log
         cat <&0 >> /tmp/x.sh.finalize.log
-        echo '{ "toolNotes" : [
-                { "type" : "type during run",
-                  "message" : "run message",
-                  "file" : "a.c"
-                  "line" : 3,
-                  "noteId" : "noteid"
-                }
-              ]}'
+        echo "" >> /tmp/x.sh.finalize.log
+        local pr_branch="v3-tool-run-and-finish-$(date '+%F_%0H-%0M-%0S_%N')"
+        echo "Checking out ${pr_branch} locally" >> /tmp/x.sh.finalize.log
+        git checkout -b $pr_branch &> /dev/null
+        echo "" >> /tmp/x.sh.finalize.log
+        cat <<EOF
+{ "toolNotes" : [
+    { "type" : "type during run",
+      "message" : "run message",
+      "file" : "a.c",
+      "line" : 3,
+      "noteId" : "noteid"
+    }
+  ]
+, "pullRequest" : {
+      "title": "x.sh PR",
+      "body": "Bug fixes and performance improvements",
+      "target_branch": "v3-tool-run-and-finish-with-finalize",
+      "source_commit": "${pr_branch}"
+  }
+}
+EOF
 }
 
 function tellName() {
